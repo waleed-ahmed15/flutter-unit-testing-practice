9ef230fa0e84692f7efa6ddb2203398b5abf2ee1
waleed-ahmed15 <waleed.ah755@gmail.com>
2023-05-29 00:52:25 +0500

    writing test cases using mocktail package

--- test/user_repository_test.dart
+++ test/user_repository_test.dart
@@ -1,7 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutterunittesting/user_model.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutterunittesting/user_repository.dart';
+import 'package:mocktail/mocktail.dart';
 
+
+class MockHTTPClient extends Mock implements http.Client {}
 void main() {
   late UserRepository userRepository;
   setUp(() {
